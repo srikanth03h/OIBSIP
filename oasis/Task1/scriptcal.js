0c7928b8a2581cf1803f52252f06fdb3c021fea8
srikanth03h <122226811+srikanth03h@users.noreply.github.com>
2023-08-14 20:17:32 +0530

    Add files via upload

---
+++ oasis/Task1/scriptcal.js
@@ -0,0 +1,19 @@
+const insertValue = value => {
+    const result = document.getElementById('result');
+    result.value += value;
+}
+
+const clearResult = () => {
+    const result = document.getElementById('result');
+    result.value = '';
+}
+
+const calculateResult = () => {
+    const result = document.getElementById('result');
+    try {
+        const calculatedResult = eval(result.value);
+        result.value = calculatedResult;
+    } catch (error) {
+        result.value = 'Error';
+    }
+}
